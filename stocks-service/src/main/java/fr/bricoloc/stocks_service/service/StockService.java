7f4a3f19194f5adf9ac75b87d77d7fe061b5facb
TheLordGibril <gabidule03@gmail.com>
2026-04-15 08:59:55 +0200

    Remove unused imports in StockService.java

--- stocks-service/src/main/java/fr/bricoloc/stocks_service/service/StockService.java
+++ stocks-service/src/main/java/fr/bricoloc/stocks_service/service/StockService.java
@@ -2,9 +2,7 @@ package fr.bricoloc.stocks_service.service;
 
 import fr.bricoloc.stocks_service.model.ToolStock;
 import fr.bricoloc.stocks_service.repository.ToolStockRepository;
-import jakarta.persistence.LockModeType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
